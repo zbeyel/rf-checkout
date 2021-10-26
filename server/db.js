7973edcc41c3d79eac3e42e6a711a3f5786057a4
zbeyel <zjabeyel@gmail.com>
2021-10-26 06:28:37 -0500

    added todo list for database interactions

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
-const { Users } = require("./models.js");
+const { Users, Hardware } = require("./models.js");
 
+//USER Database Functions
 const createUser = (userObject) => {
   let { badge_number, employee_id, first_name, last_name } = userObject;
   let newUser = new Users({
@@ -20,3 +21,23 @@ const updateUserByBadgeNumber = async (badge_number, updateToBeApplied) => {
   // updateToBeApplied is an object that will be replacing data in the found document
   await Users.updateOne({ badge_number }, updateToBeApplied);
 };
+
+const deleteUserByBadgeNumber = async (badge_number) => {
+  await Users.deleteOne({ badge_number });
+};
+
+//HARDWARE Database Functions
+//TODO Add equipment functions
+//TODO Remove equipment functions
+//TODO Edit equipment functions
+//TODO Delete equipment functions
+
+//ISSUE Functions
+//TODO Add issue query
+//TODO update existing issue query
+//TODO delete existing issue query
+
+//CheckoutLog functions
+//TODO Check Out equipment functions
+//TODO Check In equipment functions
+//TODO Check out history functions
