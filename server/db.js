6205b5ceefacfa79a57b12c4c2dd237162e49086
zbeyel <zjabeyel@gmail.com>
2021-10-25 20:23:20 -0500

    creating models for db

--- server/db.js
+++ server/db.js
@@ -3,12 +3,29 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/rf-checkout");
 
+const Units = mongoose.model("units", {
+  unit_name: String,
+  unit_type: String,
+  assigned_user: Number,
+  status: String,
+});
+
 const Users = mongoose.model("users", {
   badge_number: Number,
   employee_id: Number,
   first_name: String,
   last_name: String,
 });
+
+const Issues = mongoose.model("issue_log", {
+  keypad: Boolean,
+  notes: String,
+  other: Boolean,
+  screen: Boolean,
+  unit_number: String,
+  unresponsive: Boolean,
+});
+
 const users = new Users({
   badge_number: 9105,
   employee_id: 101661,
